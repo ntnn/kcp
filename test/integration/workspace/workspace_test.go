59464e502f2b9655971ef6988110246669db97b1
Nelo-T. Wallus <n.wallus@sap.com>
2025-04-11 16:11:54 +0200

    redo add integration test for workspace leaks
    Signed-off-by: Nelo-T. Wallus <n.wallus@sap.com>

---
+++ test/integration/workspace/workspace_test.go
@@ -0,0 +1,78 @@
+package workspace
+
+import (
+	"testing"
+	"time"
+
+	"github.com/kcp-dev/kcp/sdk/apis/core"
+	corev1alpha1 "github.com/kcp-dev/kcp/sdk/apis/core/v1alpha1"
+	tenancyv1alpha1 "github.com/kcp-dev/kcp/sdk/apis/tenancy/v1alpha1"
+	"github.com/kcp-dev/kcp/test/integration/framework"
+
+	"github.com/stretchr/testify/require"
+	"go.uber.org/goleak"
+
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/util/wait"
+)
+
+func TestWorkspaceDeletionLeak(t *testing.T) {
+	client, _ := framework.StartTestServer(t)
+
+	// orgPath, _ := framework.NewOrganizationFixture(t, server)
+
+	curGoroutines := goleak.IgnoreCurrent()
+
+	t.Logf("Create a workspace with a shard")
+	workspace, err := client.Cluster(core.RootCluster.Path()).TenancyV1alpha1().Workspaces().Create(t.Context(), &tenancyv1alpha1.Workspace{
+		ObjectMeta: metav1.ObjectMeta{Name: "ws-cleanup"},
+		Spec: tenancyv1alpha1.WorkspaceSpec{
+			Type: tenancyv1alpha1.WorkspaceTypeReference{
+				Name: "universal",
+				Path: "root",
+			},
+			Location: &tenancyv1alpha1.WorkspaceLocation{
+				Selector: &metav1.LabelSelector{
+					MatchLabels: map[string]string{
+						"name": corev1alpha1.RootShard,
+					},
+				},
+			},
+		},
+	}, metav1.CreateOptions{})
+	require.NoError(t, err, "failed to create workspace")
+
+	t.Logf("Wait until the %q workspace is ready", workspace.Name)
+	require.Eventually(t, func() bool {
+		workspace, err := client.Cluster(core.RootCluster.Path()).TenancyV1alpha1().Workspaces().Get(t.Context(), workspace.Name, metav1.GetOptions{})
+		require.NoError(t, err, "failed to get workspace")
+		if actual, expected := workspace.Status.Phase, corev1alpha1.LogicalClusterPhaseReady; actual != expected {
+			return false
+		}
+		return workspace.Status.Phase == corev1alpha1.LogicalClusterPhaseReady
+	}, 1*time.Minute, 100*time.Millisecond)
+
+	// workspaceCluster := core.RootCluster.Path().Join(workspace.Name)
+	// t.Logf("Wait for default namespace to be created")
+	// require.Eventually(t, func() bool {
+	// 	_, err := client.Cluster(workspaceCluster).CoreV1().Namespaces().Get(t.Context(), "default", metav1.GetOptions{})
+	// 	if err != nil {
+	// 		return false
+	// 	}
+	// 	return true
+	// }, 1*time.Minute, 100*time.Millisecond)
+
+	err = client.Cluster(core.RootCluster.Path()).TenancyV1alpha1().Workspaces().Delete(t.Context(), workspace.Name, metav1.DeleteOptions{})
+	require.NoError(t, err, "failed to delete workspace %s", workspace.Name)
+
+	t.Logf("Ensure workspace is removed")
+	require.Eventually(t, func() bool {
+		_, err := client.Cluster(core.RootCluster.Path()).TenancyV1alpha1().Workspaces().Get(t.Context(), workspace.Name, metav1.GetOptions{})
+		return apierrors.IsNotFound(err)
+	}, wait.ForeverTestTimeout, 100*time.Millisecond)
+
+	t.Logf("Finally check if all resources has been removed")
+
+	goleak.VerifyNone(t, curGoroutines)
+}
