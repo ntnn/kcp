91e677d7561bc7c242b72c1556c7e17167e6d85f
Nelo-T. Wallus <n.wallus@sap.com>
2026-03-17 00:56:34 +0100

    Revert "Parallelise installing root identities (#3890)"
    This reverts commit b4279ce902f927b3fb30be762a9b454859294b0b.

--- config/root-identities/bootstrap.go
+++ config/root-identities/bootstrap.go
@@ -28,7 +28,6 @@ import (
 	"k8s.io/client-go/kubernetes"
 
 	confighelpers "github.com/kcp-dev/kcp/config/helpers"
-	"github.com/kcp-dev/kcp/pkg/errgroup"
 )
 
 //go:embed *.yaml
@@ -58,18 +57,15 @@ func Bootstrap(
 		return fmt.Errorf("failed to parse bootstrap identities: %v", err)
 	}
 
-	g := errgroup.WithContext(ctx)
 	for _, identity := range identities.Identities {
-		g.Go(func(ctx context.Context) error {
-			if err := confighelpers.Bootstrap(ctx, kubeClient.Discovery(), rootDynamicClient, nil, fs, confighelpers.ReplaceOption(
-				"IDENTITY_APIEXPORT", identity.Export,
-				"IDENTITY_KEY", identity.Identity,
-			)); err != nil {
-				return fmt.Errorf("failed to bootstrap identity secret for %s: %v", identity.Export, err)
-			}
-			return nil
-		})
+		err := confighelpers.Bootstrap(ctx, kubeClient.Discovery(), rootDynamicClient, nil, fs, confighelpers.ReplaceOption(
+			"IDENTITY_APIEXPORT", identity.Export,
+			"IDENTITY_KEY", identity.Identity,
+		))
+		if err != nil {
+			return fmt.Errorf("failed to bootstrap identity secret for %s: %v", identity.Export, err)
+		}
 	}
 
-	return g.Wait()
+	return nil
 }
